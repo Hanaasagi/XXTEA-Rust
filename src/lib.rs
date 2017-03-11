49944e8d2b1db5bd0c7e7a548279cb0181f6e641
Qsaka <949241796@qq.com>
2017-03-11 22:12:35 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -16,3 +16,22 @@ fn to_bytes(v: &Vec<u32>, include_length: bool) -> Vec<u8> {
     return bytes;
 }
 
+
+fn to_u32(bytes: &Vec<u8>, include_length: bool) -> Vec<u32> {
+    let length: u32 = bytes.len() as u32;
+    let mut n: u32 = length >> 2;
+    if length&3 != 0 {
+        n = n + 1;    
+    }
+    let mut v;
+    if include_length {
+        v = vec![0; n as usize + 1];
+        v[n as usize] = length;    
+    } else {
+        v = vec![0; n as usize];    
+    }
+    for i in 0..length {
+        v[(i>>2) as usize] = (bytes[i as usize] as u32)<< ((i & 3) << 3) as u32;
+    }
+    return v
+}
